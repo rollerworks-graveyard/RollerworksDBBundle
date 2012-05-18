5b36a5bc6421cbeaa09169ec7ff714a02e0d9216
Sebastiaan Stok <s.stok@rollerscapes.net>
2012-05-18 12:56:30 +0200

    Updated coding for composer

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 
-// We should replace this with the autoloader of Composer and supply configuration for Travis
-require_once __DIR__ . '/../../../../../../app/bootstrap.php.cache';
-
-$loader->registerNamespaces(array(
-    'Rollerworks'      => __DIR__.'/../../../..',
-));
+require 'vendor/autoload.php';
